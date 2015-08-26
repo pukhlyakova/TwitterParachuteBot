eb02d7fa689a4e1b53b8d4d2adc46b8ceeaa4c56
Maria Pukhlyakova <maria.pukhlyakova@yandex.ru>
2015-08-27 00:02:03 +0300

    Add TimerRetweet
    Now it retweet status every minut

---
+++ src/RetweetStatusTimer.java
@@ -0,0 +1,64 @@
+import twitter4j.*;
+
+import java.util.*;
+
+/**
+ * This class search last status and retweet them.
+ * Created by padme on 26.08.2015.
+ */
+public class RetweetStatusTimer {
+    private Timer timer;
+    private final List<String> searchingText = new ArrayList<>();
+    {
+        searchingText.add("прыгнуть с парашютом но");
+        searchingText.add("прыгнула с парашютом но");
+        searchingText.add("прыгнул с парашютом но");
+        searchingText.add("прыгать с парашютом но");
+    }
+
+    /**
+     * Every minute gets status from twitter and retweet it if it was written in last minute.
+     */
+    public void startTimer(final Twitter twitter) {
+        timer = new java.util.Timer();
+        TimerTask task = new TimerTask() {
+            public void run()
+            {
+                retweet(twitter);
+            }
+        };
+        timer.schedule(task, 0L, 60L * 1000);
+    }
+
+    private void retweet(final Twitter twitter) {
+        Date date = Calendar.getInstance().getTime();
+        Query query = getQuery("");
+        try {
+            for (String text : searchingText) {
+                query.setQuery(text);
+                QueryResult result = twitter.search(query);
+                for (Status status : result.getTweets()) {
+                    Date statusTime = status.getCreatedAt();
+                    long ms = date.getTime() - statusTime.getTime();
+                    if (ms <= 60L * 1000) {
+                        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+                        twitter.retweetStatus(status.getId());
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } catch(TwitterException e) {
+            System.out.println(e);
+        }
+    }
+
+    private Query getQuery(String text) {
+        Query query = new Query();
+        query.setQuery(text);
+        query.setLocale("ru");
+        query.setLang("ru");
+        query.setResultType(Query.ResultType.recent);
+        return query;
+    }
+}
--- src/Twitbot.java
+++ src/Twitbot.java
@@ -1,10 +1,6 @@
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 
 /**
  * Created by padme on 23.08.2015.
@@ -12,25 +8,13 @@ import java.io.IOException;
  */
 public class Twitbot {
     public static void main(String[] args) {
-        Twitter twitter = getTwitter();
-        try {
-            Query query = new Query();
-            query.setQuery(getQueryText());
-            query.setLocale("ru");
-            query.setLang("ru");
-            query.setResultType(Query.ResultType.mixed);
-            query.setCount(100);
-            QueryResult result = twitter.search(query);
-            for (Status status : result.getTweets()) {
-                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-                twitter.retweetStatus(status.getId());
-            }
-        } catch(TwitterException e) {
-            System.out.println(e);
-        }
+        Twitbot twitbot = new Twitbot();
+        RetweetStatusTimer statusTimer = new RetweetStatusTimer();
+        Twitter twitter = twitbot.getTwitterSetting();
+        statusTimer.startTimer(twitter);
     }
 
-    private static Twitter getTwitter() {
+    private Twitter getTwitterSetting() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("*********************")
@@ -39,17 +23,5 @@ public class Twitbot {
                 .setOAuthAccessTokenSecret("*********************");
         TwitterFactory tf = new TwitterFactory(cb.build());
         return tf.getInstance();
-    }
-
-    private static String getQueryText() {
-        StringBuilder sb = new StringBuilder();
-        try(BufferedReader br = new BufferedReader(new FileReader("E:\\Projects\\TwitterParachuteBot\\src\\query.txt"))) {
-            String line = br.readLine();
-            sb.append(line);
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
-        return sb.toString();
     }
 }
